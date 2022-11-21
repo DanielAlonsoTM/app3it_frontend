4b0d7b64bedeb84ae32343d3d261d2fd5b48e229
Daniel Tapia Maldonado <47828664+DanielAlonsoTM@users.noreply.github.com>
2022-11-21 12:38:56 -0300

    Connection to backend

---
+++ src/app/services/PollService.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Poll } from "../shared/poll";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PollService {
+    private baseURL = 'http://localhost:8080/polls';
+
+    constructor(private http: HttpClient) { }
+
+    private httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+
+    addPoll(data: any): Observable<Poll> {
+        return this.http
+            .post<Poll>(
+                this.baseURL + '/add',
+                JSON.stringify(data),
+                this.httpOptions);
+    }
+
+    getResults(): Observable<Poll[]> {
+        return this.http
+            .get<Poll[]>(
+                this.baseURL,
+                this.httpOptions
+            );
+    }
+}
